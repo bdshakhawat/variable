948a701e09f31fdc302fa4aa6eaa35f1347f5d6e
bdshakhawat <mdshakwat.hossain@gmail.com>
2023-09-20 17:42:30 +0600

    first commit

---
+++ variabls-problems/varible.js
@@ -0,0 +1,77 @@
+// Problem 1:Declare a variabl named 'greetings' and
+// 'name' and assign the value 'Hello world' and 
+// Log the result to the console.
+
+// let greetings = "Hello World";
+// console.log(greetings);
+// ----------------------------------------------
+// Problem 2:Declare two variables a and b. Assign them values of 5 and 10 respectively. Calculate their sum, difference, 
+// product, and quotient, and store each result in separate variables.
+
+
+// let a=5;
+// let b=10;
+// let sum=a+b;
+// let difference=a-b;
+// let product=a*b;
+// let quotient=a/b;
+// console.log(sum);
+// console.log(difference);
+// console.log(product);
+// console.log(quotient);
+// 15, -5, 50, 0.5
+// ----------------------------------------------
+
+// Problem 3:Without using a third variable, swap the values 
+// of two variables x and y
+// let x=10;
+// let y=5;
+
+// Swaping using arithmetic operators
+
+// x=x+y;
+// y=x-y;
+// x=x-y;
+// console.log(x, y);
+// output: 5, 10
+
+// alternative solution:
+let x=10;
+let y=5;
+console.log("Before swap: x=", x,", y=" ,  y);
+// Before swap: x= 10 , y= 5
+[x,y]=[y,x];
+console.log("After swap: x=", x,", y=" ,  y);
+// After swap: x= 5 , y= 10
+
+// 2nd alternative solution:
+// let x=10;
+// let y=5;
+// console.log("Before swap: x=", x,", y=" ,  y);
+// Before swap: x= 10 , y= 5
+// let temp=x;
+// x=y;
+// y=temp;
+// console.log("After swap: x=", x,", y=" ,  y);
+// After swap: x= 5 , y= 10
+
+// 3rd alternative solution:
+
+// let x=10;
+// let y=5;
+// console.log("Before swap: x=", x,", y=" ,  y);
+// Before swap: x= 10 , y= 5
+// let temp;
+// temp=x;
+// x=y;
+// y=temp;
+// console.log("After swap: x=", x,", y=" ,  y);
+// After swap: x= 5 , y= 10
+
+
+
+
+
+// ----------------------------------------------
+
+
